1753da2569982e06372d93b864b7e25d85021a0f
Danilo Oliveira <danilooliveira9@hotmail.com>
2017-12-28 03:41:31 +0000

    remove obj and bin and add BearerTest

---
+++ BearerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BearerAuthentication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicHubBusiness.Business;
+using MusicHubBusiness.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BearerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string email = "danilooliveira9@hotmail.com";
+            string password = "031996";
+
+            MusicianBusiness musicianBusiness = new MusicianBusiness();
+
+            Musician user = musicianBusiness.Login(email, password);
+
+            BearerToken bearerLogin = new BearerToken();
+            bearerLogin.GenerateHeaderToken(user.id.ToString(), user.email);
+        }
+    }
+}
