77c29d86e7bd0e431e11d9496a57477bc8b72003
Danilo Oliveira <danilooliveira9@hotmail.com>
2018-01-10 03:09:12 +0000

    add tests for musical project

--- BaseTest.cs
+++ BaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using Bogus;
+using MusicHubBusiness.Business;
+using MusicHubBusiness.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,33 @@ namespace MusicHubTest
     public class BaseTest
     {
         public Faker faker { get; set; }
+        public Musician loggedUser { get; set; }
 
         public BaseTest()
         {
             this.faker = new Faker("pt_BR");
+        }
+
+        public void Login()
+        {
+            string name = "Teste User", email = "teste.user@musichub.com", password = "#Danil123";
+
+            MusicianBusiness musicianBusiness = new MusicianBusiness();
+            Musician musician = musicianBusiness.Login(email, password);
+
+            if(musician == null)
+            {
+                musician = new Musician
+                {
+                    name = name,
+                    email = email,
+                    password = password
+                };
+
+                musician = musicianBusiness.Create(musician);
+            }
+
+            loggedUser = musician;
         }
     }
 }
---
+++ MusicalProjectTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MusicHubBusiness.Business;
+using MusicHubBusiness.Models;
+using MusicHubTest;
+using System.IO;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MusicalProjectTest : BaseTest
+    {
+        //[TestMethod]
+        //public void MusicalProject_Create()
+        //{
+        //    Login();
+
+        //    MockRepository mocks = new MockRepository(MockBehavior.Default);
+        //    var basket = mocks.Set Create<FileStream>();
+
+        //    var stream = basket.Object;
+
+        //    MusicalGenreBusiness musicalGenreBusiness = new MusicalGenreBusiness();
+        //    var musical_genre = musicalGenreBusiness.GetAll().First();
+
+        //    MusicalProjectBusiness musicalProjectBusiness = new MusicalProjectBusiness();
+        //    MusicalProject musicalProject = new MusicalProject
+        //    {
+        //        name = faker.Lorem.Sentence(15),
+        //        musical_genre_id = musical_genre.id,
+        //        owner_id = loggedUser.id
+        //    };
+
+        //    musicalProjectBusiness.Create(musicalProject, stream);
+        //}
+    }
+}
